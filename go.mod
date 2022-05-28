b65f21d9795e4f86540eca10666e42f7fba9c265
enggar tivandi <enggartivandi@outlook.com>
2022-05-29 07:06:50 +0800

    chore: implementasi golang module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nekoding/go-hello
+
+go 1.18
+
+require github.com/nekoding/go-mod-test v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/nekoding/go-mod-test"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("Enggar"))
+	fmt.Println(go_say_hello.SayHello("Tivandi"))
+}
+
+
